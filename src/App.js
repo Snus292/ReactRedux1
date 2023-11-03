fc024e7e8dc7f7db11a4339adc3d96c010891000
Snus292 <georgiigiblov@gmail.com>
2023-11-03 02:49:45 +0200

    изменина файловая иерархия ,устранены ошибки с путями, обновленные зависимости

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from "/ReactRedux/react-redux1/src/logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { createStore } from "redux";
-import { createRoot } from 'react-dom/client';
 
 // начиная с версии React 18, метод ReactDOM.render больше не 
 // поддерживается, и вместо него рекомендуется использовать 
@@ -65,13 +68,14 @@ document.getElementById("rnd").addEventListener("click", () => {
 // state =reducer(state, {type: "INC"});
 // console.log(state);
 
-const root = createRoot(document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <>
-
-    </>
-  </React.StrictMode>,
-  
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
